229feffe993329134d836b7d811e81b2a6df7498
John Huang <johnhuang1225@gmail.com>
2015-05-16 16:00:37 +0800

    DBFM init

---
+++ DBFM/DBFM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DBFM
+//
+//  Created by John Huang on 2015/5/16.
+//  Copyright (c) 2015年 JohnHuang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
